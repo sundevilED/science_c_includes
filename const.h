5bcdb99b43770d5f813eba032ae15d176fcd288a
Edgar Manriquez <39634427+sundevilED@users.noreply.github.com>
2019-02-07 12:07:34 -0700

    Update const.h

--- const.h
+++ const.h
@@ -3,63 +3,54 @@
 
 /* Universal Constants in SI Units */
 
-#define c               299792458       /* Speed of Light in Vaccum [m*s^-1] */
+#define C               299792458       /* Speed of Light in Vaccum [m*s^-1] */
 #define G               6.67408E-11     /* Gravitational Constant [m^3*kg^-1*s^-2]*/
-#define h               6.626070040E-34 /* Planck Constant [J*s] */
-#define h_bar           1.054571800E-34 /* Reduced Planck Constant [J*s] */
+#define H               6.626070040E-34 /* Planck Constant [J*s] */
+#define H_BAR           1.054571800E-34 /* Reduced Planck Constant [J*s] */
 
-#define l_P             1.616229E-35    /* Planck Length [m] */
-#define m_P             2.176470E-8     /* Planck Mass [kg] */
-#define t_P             5.39116E-44     /* Planck Time [s] */
-#define q_P             1.875545956E-18 /* Planck Charge [C] */
+#define L_P             1.616229E-35    /* Planck Length [m] */
+#define M_P             2.176470E-8     /* Planck Mass [kg] */
+#define TIME_P          5.39116E-44     /* Planck Time [s] */
+#define Q_P             1.875545956E-18 /* Planck Charge [C] */
 #define T_P             1.416808E32     /* Planck Temperature [K] */
 
-#define mu_0            12.566370614E-7 /* Vacuum Permeability [N*A^-2] */
-#define mu_B            927.4009994E-26 /* Bohr Magneton [J*T^-1] */
-#define epsilon_0       8.854187817E-12 /* Vaccum Permittivity [F*m^-1] */
+#define MU_0            12.566370614E-7 /* Vacuum Permeability [N*A^-2] */
+#define MU_B            927.4009994E-26 /* Bohr Magneton [J*T^-1] */
+#define EPSILON_0       8.854187817E-12 /* Vaccum Permittivity [F*m^-1] */
 #define Z_0             376.730313461   /* Impedence of a Vaccum [Ohm] */
-#define k_e             8.98755178736E9 /* Coulomb's Constant [kg*m^3*s^−4*A^−2] */
-#define bohr_r          5.291772106E-11 /* Bohr Radius [m] */
+#define K               8.98755178736E9 /* Coulomb's Constant [kg*m^3*s^−4*A^−2] */
+#define BOHR_R          5.291772106E-11 /* Bohr Radius [m] */
 
-#define e               1.602176620E-19 /* Electron Charge [C] */
-#define m_e             9.109390E-31    /* Mass Electron [kg] */
-#define m_p             1.672623E-27    /* Mass Proton [kg] */
-#define m_n             1.674929E-27    /* Mass Neutron [kg] */
-#define amu             1.660539040E-27 /* Atomic Mass Unit [kg} */
+#define E               1.602176620E-19 /* Electron Charge [C] */
+#define MASS_E          9.109390E-31    /* Mass Electron [kg] */
+#define MASS_P          1.672623E-27    /* Mass Proton [kg] */
+#define MASS_N          1.674929E-27    /* Mass Neutron [kg] */
+#define AMU             1.660539040E-27 /* Atomic Mass Unit [kg} */
 #define NA              6.022140857E23  /* Avogadro Constant [mol^-1] */
-#define kB              1.38064852E-23  /* Boltzmann Constant [J*K^-1] */
+#define KB              1.38064852E-23  /* Boltzmann Constant [J*K^-1] */
 #define R               8.3144598       /* Universal Gas Constant [J*mol^-1*K^-1]*/
-
-/* Mathematical Constants */
-
-#define pi              3.14159265358979 /* Ratio of all circle's circumference and diameter */
-#define exp             2.71828182845904 /* Euler's Number */
-#define phi             1.61803398874989 /* Golden Ratio */
-#define root2           1.41421356237309 /* Square Root of 2 */
-#define root3           1.73205080756887 /* Square Root of 3 */
-#define root5           2.23606797749978 /* Square Root of 5 */
 
 /* Order of Magnitude Conversion */
 
-#define yocto           1E-24
-#define zepto           1E-21
-#define atto            1E-18
-#define femto           1E-15
-#define pico            1E-12
-#define nano            1E-9
-#define micro           1E-6
-#define milli           1E-3
-#define centi           1E-2
-#define deci            1E-1
-#define ten             1E1
-#define deca            1E2
-#define kilo            1E3
-#define mega            1E6
-#define giga            1E9
-#define tera            1E12
-#define peta            1E15
-#define exa             1E18
-#define zetta           1E21
-#define yotta           1E24
+#define YOCTO           1E-24
+#define ZEPTO           1E-21
+#define ATTO            1E-18
+#define FEMTO           1E-15
+#define PICO            1E-12
+#define NANO            1E-9
+#define MICRO           1E-6
+#define MILLI           1E-3
+#define CENTI           1E-2
+#define DECI            1E-1
+#define TEN             1E1
+#define DECA            1E2
+#define KILO            1E3
+#define MEGA            1E6
+#define GIGA            1E9
+#define TERA            1E12
+#define PETA            1E15
+#define EXA             1E18
+#define ZETTA           1E21
+#define YOTTA           1E24
 
 #endif
